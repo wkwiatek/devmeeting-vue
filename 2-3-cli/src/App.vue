1abd6f340508f44131cdb8055efcd28955a81842
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2018-03-09 00:21:56 +0100

    Unit 2

---
+++ 2-3-cli/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h2>My awesome list</h2>
+    <ul>
+      <li v-for="p in products">{{ p.name }}</li>
+    </ul>
+    <p v-if="!products.length">No products!</p>
+
+    <!-- 1. Vue gives as some nice syntax to cope with casual use cases -->
+    <form @submit.prevent="onSubmit()">
+      <!-- 2. Any Angular fan here? v-model make a binding with defined in data object -->
+      <input v-model="newProduct.name">
+      <button>Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import uuid from 'uuid/v4';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      products: [{
+        id: 0,
+        name: 'Coffee'
+      }, {
+        id: 1,
+        name: 'Pizza'
+      }],
+      //3/ 3. Here goes the definition
+      newProduct: {
+        name: ''
+      }
+    }
+  },
+  methods: {
+    //7/ 4. By keeping reference to data we can let Vue to do it's job
+    onSubmit() {
+      this.products.push({
+        id: uuid(),
+        ...this.newProduct
+      });
+      this.newProduct.name = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
