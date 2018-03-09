0e1b3457c270490515f295ff69bd5214048c2c2d
User for ghslides web application <xpla-ghslides@xpla.org>
2018-03-09 06:12:46 -0500

    Auto-generated slides.

---
+++ 5-2-vuex/001/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h2>My awesome list</h2>
+
+    <product-list :products="products"></product-list>
+    <add-product @onAddProduct="handleAddProduct"></add-product>
+
+  </div>
+</template>
+
+<script>
+import ProductList from '../components/ProductList';
+import AddProduct from '../components/AddProduct';
+
+export default {
+  name: 'app',
+  components: {
+    ProductList,
+    AddProduct
+  },
+  //3/ When the component is created we're immediately dispatching an action
+  created() {
+    this.$store.dispatch('getProducts');
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  methods: {
+    handleAddProduct(product) {
+      this.$store.dispatch('addProduct', product);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
