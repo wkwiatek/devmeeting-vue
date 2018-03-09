08879aef5c752718a12b7427287a4a943703463d
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2018-03-09 01:17:27 +0100

    Unit 3

---
+++ 3-3-comp/components/AddProduct.vue
@@ -0,0 +1,50 @@
+<template>
+  <form @submit.prevent="onSubmit()">
+    <input
+      name="product"
+      v-model="newProduct.name"
+      v-validate="'required|min:3'"
+    >
+    <button>Add</button>
+    <div v-show="errors.has('product')">
+      {{ errors.first('product') }}
+    </div>
+  </form>
+</template>
+
+<script>
+  import uuid from 'uuid/v4';
+
+  export default {
+    name: "AddProduct",
+    //6/ newProduct goes to AddProduct component
+    data() {
+      return {
+        newProduct: {
+          name: ''
+        }
+      }
+    },
+    //3/ As well as onSubmit
+    methods: {
+      onSubmit() {
+        this.$validator.validateAll().then(result => {
+          if (!result) {
+            return;
+          }
+          //4/ But instead of adding element we're just emitting onAddProduct event with product as a payload
+          this.$emit('onAddProduct', {
+            id: uuid(),
+            ...this.newProduct
+          });
+          this.newProduct.name = '';
+          this.$validator.reset();
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 3-3-comp/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h2>My awesome list</h2>
+
+    <product-list :products="products"></product-list>
+    <!-- With @ we can listen to onAddProduct event and assign handleAddProduct-->
+    <add-product @onAddProduct="handleAddProduct"></add-product>
+
+  </div>
+</template>
+
+<script>
+import ProductList from '../components/ProductList';
+import AddProduct from '../components/AddProduct';
+
+export default {
+  name: 'app',
+  components: {
+    ProductList,
+    AddProduct
+  },
+  data() {
+    return {
+      products: [{
+        id: 0,
+        name: 'Coffee'
+      }, {
+        id: 1,
+        name: 'Pizza'
+      }]
+    }
+  },
+  methods: {
+    //3/ All we have to do in a method is to add product to the list
+    handleAddProduct(product) {
+      this.products.push(product);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
